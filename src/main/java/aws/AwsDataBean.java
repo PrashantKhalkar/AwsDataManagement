61b971692847f296689410a3a27bb799fab268bb
prashant <Prashant.khalkar@cognizant.com>
2018-08-13 19:15:21 +0530

    Initial setup

---
+++ src/main/java/aws/AwsDataBean.java
@@ -0,0 +1,21 @@
+package aws;
+
+
+class AwsDataBean {
+	
+	private String topicName;
+	private String bookName;
+	
+	public String getTopicName() {
+		return topicName;
+	}
+	public void setTopicName(String topicName) {
+		this.topicName = topicName;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+}
---
+++ src/main/java/aws/WebController.java
@@ -0,0 +1,22 @@
+package aws;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class WebController {
+
+	@RequestMapping(value = "/getData")
+	public ResponseEntity<AwsDataBean> welcome() {
+		AwsDataBean bean = new AwsDataBean();
+		bean.setBookName("AWS");
+		bean.setTopicName("S3 Bucket");
+		return new ResponseEntity<AwsDataBean>(bean,HttpStatus.OK);
+	}
+
+}
